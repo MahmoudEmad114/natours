5c90752e6c06ef47674976954ea4a4391ea3465e
mahmoud <sci.MahmoudEmad77352@alexu.edu.eg>
2026-08-25 19:57:09 +0300

    Back to sendGrid emails

--- utils/email.js
+++ utils/email.js
@@ -1,8 +1,6 @@
-const { Resend } = require('resend');
+const nodemailer = require('nodemailer');
 const pug = require('pug');
 const htmlToText = require('html-to-text');
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 module.exports = class Email {
     constructor(user, url) {
@@ -12,41 +10,52 @@ module.exports = class Email {
         this.from = `Natours <${process.env.EMAIL_FROM}>`;
     }
 
+    newTransport() {
+        if (process.env.NODE_ENV === 'production ') {
+            // Sendgrid
+            return nodemailer.createTransport({
+                service: 'SendGrid',
+                auth: {
+                    user: process.env.SENDGRID_USERNAME,
+                    pass: process.env.SENDGRID_PASSWORD
+                }
+            });
+        }
+
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+    }
+
+    // Send the actual email
     async send(template, subject) {
         // 1) Render HTML based on a pug template
-        const html = pug.renderFile(
-            `${__dirname}/../views/email/${template}.pug`,
-            {
-                firstName: this.firstName,
-                url: this.url,
-                subject
-            }
-        );
+        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        });
 
-        // 2) Send email using Resend
-        const { data, error } = await resend.emails.send({
+        // 2) Define email options
+        const mailOptions = {
             from: this.from,
-            to: [this.to],
+            to: this.to,
             subject,
             html,
             text: htmlToText.convert(html)
-        });
+        };
 
-        // 3) Handle Resend error
-        if (error) {
-            console.error('RESEND ERROR:', error);
-            throw new Error(error.message);
-        }
-
-        console.log('Email sent successfully:', data.id);
-        return data;
+        // 3) Create a transport and send email
+        await this.newTransport().sendMail(mailOptions);
     }
 
     async sendWelcome() {
-        await this.send(
-            'welcome',
-            'Welcome to the Natours family!'
-        );
+        await this.send('welcome', 'Welcome to the Natours Family!');
     }
 
     async sendPasswordReset() {
